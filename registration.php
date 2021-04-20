cff64835d74682d79888df0ba9ff57289515edf9
vuphuclam198 <vuphuclam98@gmail.com>
2021-04-20 13:36:55 +0700

    change registration

--- registration.php
+++ registration.php
@@ -6,4 +6,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AHT/burning', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/AHT/mkod', __DIR__);
